5ca6b9231c7836ff8d57d5a8e25f04e9782fce94
PAL-LIAMENT <kimminjae727@naver.com>
2024-11-15 14:08:19 +0900

    sh

--- 5.network_syntax.sh
+++ 5.network_syntax.sh
@@ -20,4 +20,24 @@ sudo systemctl start nginx
 sudo apt-get install openjdk-11-jdk
 
 # 네트워크 관련 명령어
-# 네트워크 설정 정보 조회
+# 네트워크 설정 정보 조회 : 윈도우에서는 ipconfig
+ifconfig
+# 특정 도메인의 IP주소 정보 조회
+nslookup <도메인주소>
+# 네트워크 연결상태 조회 (ip만 사용)
+# 일반적으로 ping은 보안상 막아두고 있음에 유의
+ping <IP주소>
+ping 8.8.8.8
+ping 네이버IP
+# 네트워크 연결상태 조회 (port까지 사용하여 조회) (net cat)
+nc -zv <ip 또는 도메인> <포트>
+nc -zv naver.com 443
+
+
+# 원격접속 : 22번 포트
+ssh <username>@<host(컴퓨터)IP주소>
+# 원격으로 파일 전송
+scp <전송하고자하는파일> <원격지주소>
+
+# 현재 서버에 연결된 네트워크 정보 출력
+netstat
